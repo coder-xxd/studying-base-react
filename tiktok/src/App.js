cd9ca2374d53c0db515962d0eeb7dd00714341b8
coder-xxd <xxd.danny.pt02@gmail.com>
2021-10-31 16:39:18 +0700

    upload

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -7,9 +7,9 @@ import { useState } from 'react';
 const  orders = [100, 200, 300]
 
 function App() {
+  // useState dung voi callback
   const [counter, setCounter] = useState(() => {
     const total = orders.reduce((total, cur) => total + cur)
-
     console.log(total)
     return total
   })
@@ -18,10 +18,25 @@ function App() {
     setCounter(counter + 1)
   }
 
+  const [info, setInfo] = useState({
+    name: 'Nguyen Van A',
+    age: 18,
+    address: 'Nha Trang'
+  })
+  function handleUpdate() {
+    setInfo({
+      ...info,
+      name: 'Danny Nguyen',
+      bio:'Love basketball and money ^^'
+    })
+  }
+
   return (
     <div className="App" style={{padding : 20}}>
       <h1 >{counter}</h1>
       <button onClick={handleIncrease} >Increase</button>
+      <h1 >{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate} >Update</button>
     </div>
   );
 }
